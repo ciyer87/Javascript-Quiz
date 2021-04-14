630de2be18d430579ebf03c6b5a02a31cc9c27d4
ciyer87 <chitra.iyer00@gmail.com>
2021-04-13 21:29:54 -0700

    added comments, fixed css

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,8 +16,10 @@ var quizEl = document.querySelector('#quiz-body');
 var statusEl = document.querySelector('#status');
 var timeInterval;
 var timeLeft = 0;
-// this is a multidimensional array with 5 inner array elements with 6 elements inside them 
+var formEl = document.querySelector("#highscore");
+var highscoreEl = document.querySelector("#highscore-panel")
 
+// this is a multidimensional array with 5 inner array elements with 6 elements inside them 
 var myQuestions = [
     {
         question: "Commonly used data types do NOT include:",
@@ -69,12 +71,13 @@ var myQuestions = [
 ];
 
 // this get function is short for the getElementById function  
+
 function get(x) {
     return document.getElementById(x);
 }
-quizEl.textContent = "Try to answer the following code - related questions withn the time limit.Keep in mind that incorrect answers will penalize your score / time by ten seconds!";
+quizEl.textContent = "Try to answer the following code related questions within the time limit.  Keep in mind that incorrect answers will penalize your score / time by ten seconds!";
 
-
+// timer function to run in parallel with the quiz
 function timer() {
     timeLeft = 60;
     startBtn.setAttribute("style", "display: none"); // Hide start button on start
@@ -96,12 +99,13 @@ function timer() {
 }
 
 // this function renders a question for display on the page
-
 function renderQuestion() {
     test = get("quiz-body");
     if (pos >= myQuestions.length) {
         endGame(timeLeft);
+        //displays the total number of correct answers to users
         get("status").innerHTML = "<h2>You got " + correct + " of " + myQuestions.length + " questions correct</h2>";
+        //displays the final score on top
         test.innerHTML = "Test completed.  Your final score is: " + timeLeft;
         // resets the variable to allow users to restart the test
         pos = 0;
@@ -109,10 +113,11 @@ function renderQuestion() {
         // stops rest of renderQuestion function running when test is completed
         return false;
     }
-
+    //displays which question the user is currently looking at
     get("quiz-body").innerHTML = "Question " + (pos + 1) + " of " + myQuestions.length;
-
+    //iterate through the questions based on the position (quiz number)
     question = myQuestions[pos].question;
+    //fetch the multiple choice answers
     chA = myQuestions[pos].a;
     chB = myQuestions[pos].b;
     chC = myQuestions[pos].c;
@@ -128,6 +133,7 @@ function renderQuestion() {
     test.innerHTML += "<button onclick='checkAnswer()'>Submit Answer</button>";
 }
 
+//funciton to validate the users repsonse with actual response
 function checkAnswer() {
     // use getElementsByName because we have an array which it will loop through
     choices = document.getElementsByName("choices");
@@ -141,19 +147,63 @@ function checkAnswer() {
         //each time there is a correct answer this value increases
         correct++;
     }
+    else
+        //reduce 10 secs to penalize wrong answer
+        timeLeft = timeLeft - 10;
     // changes position of which character user is on
     pos++;
-
     // then the renderQuestion function runs again to go to next question
     renderQuestion();
 }
-// Add event listener to call renderQuestion on page load event
-//window.addEventListener("load", renderQuestion);
 
+//endGame function to clear timer and display message to users if time runs out before quiz is completed
 var endGame = function (timeLeft) {
-    console.log("time left in endGame ", timeLeft);
     clearInterval(timeInterval); // Stop Timer
     quizEl.innerHTML = "<h2>All Done!</h2><h3>Your final score is " + (timeLeft) + "</h3>";
+    var highscoreEl = document.querySelector('#highscore-panel');
+    highscoreEl.setAttribute("style", "display: block; margin-top: -250px");
+}
+
+//function to record highscore of the user by using local storage
+var recordHighScores = function () {
+    event.preventDefault();
+    var playerInput = document.querySelector("input[name='player-initials']").value;
+    // Check if input values are empty strings
+    if (!playerInput) {
+        alert("Please enter your name or initals!");
+        return false;
+    }
+    // clear the screen
+    highscoreEl.textContent = "";
+    quizEl.textContent = "";
+    statusEl.textContent = "";
+
+    // Gather data for local storage
+    var highscore = {
+        name: playerInput,
+        score: timeLeft
+    }
+    localStorage.setItem('scores', JSON.stringify(highscore));
+    showHighScores();
+};
+
+//function to display highscore from local storage
+var showHighScores = function () {
+    var savedScores = localStorage.getItem("scores")
+    //if there are no high scores, display below message
+    if (!savedScores) {
+        alert("No High Scores Recorded!")
+        return false;
+    }
+    savedScores = JSON.parse(savedScores);
+    //display score.  when user clicks on go back, restart quiz. clear high score clears the score from local storage
+    quizEl.innerHTML = "<h2 class='score-header'>High Scores!</h2>" +
+        "<div class='score-list'>1) " + savedScores.name + " &ndash; " + savedScores.score + "</div>" +
+        "<div class='score-buttons'>" +
+        "<button class='btn' onclick='location.reload()'>Go Back</button>" +
+        "<button class='btn' onclick='localStorage.clear()'>Clear High Scores</button>" +
+        "</div>";
 }
 
 startBtn.onclick = timer;
+formEl.addEventListener("submit", recordHighScores);
